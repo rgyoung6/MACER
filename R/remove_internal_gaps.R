9fd04d0f338b24fa3df49ab9db9bcfa8513d87a5
rgyoung6 <rgyoung6@gmail.com>
2021-06-17 12:01:56 -0400

    Create remove_internal_gaps.R

---
+++ R/remove_internal_gaps.R
@@ -0,0 +1,36 @@
+#Roxygen2 Documentation:
+
+#' @keywords internal
+#'
+#' @author Robert G. Young
+#'
+#' @references
+#' https://github.com/rgyoung6/MACER
+#' Young, R. G., Gill, R., Gillis, D., Hanner, R. H. (Submitted June 2021). Molecular Acquisition, Cleaning, and Evaluation in R (MACER) - A tool to assemble molecular marker datasets from BOLD and GenBank. Biodiversity Data Journal.
+#'
+
+# Identify and remove internal gaps
+############################## remove_internal_gaps FUNCTION ############################
+remove_internal_gaps <- function(output_fasta_matrix, pigl){
+
+  #initalizing the variable
+  col_to_remove = NULL
+
+  #finding columns with the number sequences with a gap at that position greater than the internal gap percentage, then remove the sequences with
+  # nucleotides causing that internal gap and then add the column to a vector for removal as it would now only have gaps
+  for(k in 3:ncol(output_fasta_matrix)){
+    if(nrow(subset(output_fasta_matrix, (output_fasta_matrix[,k]=="-")|(output_fasta_matrix[,k]=="N")))>(nrow(output_fasta_matrix)*pigl)){
+      #remove the sequence(s) with the nucleotide
+      output_fasta_matrix<-subset(output_fasta_matrix, (output_fasta_matrix[,k]=="-")|(output_fasta_matrix[,k]=="N"))
+      #setting up a vector to hold all of the rows to be removed note that I am not subtracting 1 as I need to account for the row with the header
+      col_to_remove<-c(col_to_remove,(k))
+    }
+  }
+  if (!is.null(col_to_remove)){
+    #deleting the internal gap sequences
+    output_fasta_matrix<-output_fasta_matrix[,-c(col_to_remove)]
+  }
+
+  return(output_fasta_matrix)
+
+}
